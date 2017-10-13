d1229ca64326762cb4c678e35e5d89169122c0a6
Michael Marcin <mike.marcin@gmail.com>
2017-10-13 11:28:59 -0500

    should return partition points end iterator

--- benchmarks/algorithm_bench.cpp
+++ benchmarks/algorithm_bench.cpp
@@ -1,39 +1,39 @@
 /*
-Run on( 8 X 4008 MHz CPU s )
-10 / 13 / 17 02:37 : 48
+Run on (8 X 4008 MHz CPU s)
+10/13/17 11:21:46
 ------------------------------------------------------------------
 Benchmark                           Time           CPU Iterations
 ------------------------------------------------------------------
-StableSort / 5 / 64                   805 ns        858 ns     746667
-StableSort / 5 / 512                 7372 ns       7150 ns      89600
-StableSort / 5 / 4096               68138 ns      71933 ns      13033
-StableSort / 5 / 32768             694004 ns     703125 ns       1000
-StableSort / 5 / 262144           6071027 ns    5937500 ns        100
-StableSort / 20 / 64                  907 ns        830 ns     640000
-StableSort / 20 / 512                9292 ns      10254 ns      64000
-StableSort / 20 / 4096              84552 ns      83705 ns       8960
-StableSort / 20 / 32768            818910 ns     857597 ns        747
-StableSort / 20 / 262144          7122335 ns    7254464 ns        112
-StableSort / 100 / 64                 934 ns        903 ns     640000
-StableSort / 100 / 512              11332 ns      11719 ns      56000
-StableSort / 100 / 4096            108316 ns      89286 ns       5600
-StableSort / 100 / 32768           994719 ns     920759 ns        560
-StableSort / 100 / 262144         8570463 ns    7986111 ns         90
-StableSegregate / 5 / 64              901 ns        900 ns     746667
-StableSegregate / 5 / 512            4617 ns       3868 ns     145438
-StableSegregate / 5 / 4096          33847 ns      36272 ns      17231
-StableSegregate / 5 / 32768        263493 ns     268555 ns       3200
-StableSegregate / 5 / 262144      2235997 ns    2064732 ns        280
-StableSegregate / 20 / 64            1633 ns       1569 ns     448000
-StableSegregate / 20 / 512          10111 ns      10045 ns      74667
-StableSegregate / 20 / 4096         62014 ns      73242 ns       8960
-StableSegregate / 20 / 32768       454030 ns     429086 ns       1493
-StableSegregate / 20 / 262144     4018690 ns    4269622 ns        172
-StableSegregate / 100 / 64           1579 ns       1496 ns     407273
-StableSegregate / 100 / 512         30081 ns      28878 ns      24889
-StableSegregate / 100 / 4096       166012 ns     172635 ns       4978
-StableSegregate / 100 / 32768     1203770 ns    1296854 ns        747
-StableSegregate / 100 / 262144   10426475 ns   11041667 ns         75
+StableSort/5/64                   808 ns        774 ns     746667
+StableSort/5/512                 7348 ns       7499 ns      89600
+StableSort/5/4096               68220 ns      64174 ns      11200
+StableSort/5/32768             683621 ns     593750 ns       1000
+StableSort/5/262144           6118716 ns    6250000 ns         90
+StableSort/20/64                  908 ns        900 ns     746667
+StableSort/20/512                9294 ns      10498 ns      64000
+StableSort/20/4096              84527 ns      87847 ns       9249
+StableSort/20/32768            812946 ns     899431 ns        747
+StableSort/20/262144          7114586 ns    7393973 ns        112
+StableSort/100/64                 932 ns        879 ns    1120000
+StableSort/100/512              11306 ns      10930 ns      74335
+StableSort/100/4096            108063 ns      97656 ns       6400
+StableSort/100/32768           984662 ns    1025391 ns        640
+StableSort/100/262144         8562026 ns    8333333 ns         75
+StableSegregate/5/64              892 ns        753 ns     746667
+StableSegregate/5/512            4614 ns       4290 ns     149333
+StableSegregate/5/4096          33644 ns      30134 ns      24889
+StableSegregate/5/32768        266188 ns     262277 ns       2800
+StableSegregate/5/262144      2265832 ns    2083333 ns        345
+StableSegregate/20/64            1635 ns       1674 ns     373333
+StableSegregate/20/512          10189 ns      10254 ns      74667
+StableSegregate/20/4096         59508 ns      52828 ns      11535
+StableSegregate/20/32768       455795 ns     435287 ns       1723
+StableSegregate/20/262144     4048701 ns    3765060 ns        166
+StableSegregate/100/64           1560 ns       1674 ns     746667
+StableSegregate/100/512         30493 ns      32959 ns      21333
+StableSegregate/100/4096       166070 ns     163218 ns       4978
+StableSegregate/100/32768     1199679 ns    1129518 ns        498
+StableSegregate/100/262144   10730091 ns   10625000 ns         75
 */
 
 #include <random>
@@ -64,33 +64,31 @@ public:
  * elements match if they compare equal using operator== or pred
  *
  * \param f, l	            the range of elements to segregate
- * \param partition_points  an output iterator that each partition point is written to
+ * \param partition_points  beginning of the destination that will store all partition points
  * \param Pred              binary predicate which returns true for matching elements
  *
- * \returns the number of partition points
+ * \returns the new end of the partition points range
  *
  * \pre [f,l) is a valid range
  * \note *f will not move
   * \note an earlier name was stable_adjacent_partition
  */
-template< typename I, typename O, typename Pred >
-size_t stable_segregate( I f, I l, O partition_points, Pred p )
+template< typename BidirIt, typename OutIt, typename BinaryPred >
+OutIt stable_segregate( BidirIt f, BidirIt l, OutIt partition_points, BinaryPred p )
 {
-    size_t num_partition_points = 0;
     while ( f != l ) {
         auto & q = *f;
         auto is_match = [&]( auto && x ) -> bool { return p(q,x); };
         f = std::stable_partition( ++f, l, is_match );
         *partition_points = f;
         ++partition_points;
-        ++num_partition_points;
     }
 
-    return num_partition_points;
+    return partition_points;
 }
 
-template< typename I, typename O >
-size_t stable_segregate( I f, I l, O partition_points )
+template< typename BidirIt, typename OutIt >
+OutIt stable_segregate( BidirIt f, BidirIt l, OutIt partition_points )
 {
     return stable_segregate( f, l, partition_points, std::equal_to<>() );
 }
